8ceaebbe57abd9a9f9f46d268de9ba6e70877195
castle hyeon <qnftktls118@naver.com>
2022-04-18 16:26:10 +0900

    0418 commit

--- posts/admin.py
+++ posts/admin.py
@@ -8,7 +8,11 @@ class PostModelAdmin(admin.ModelAdmin):
     #(괄호 안에는 모델의 속성명을 넣어주는 것.)
     list_display = ('id', 'image', 'content', 'created_at', 'view_count', 'writer')
     #list_editable = ('content') -> 리스트 화면에서 수정이 바로 가능하다.
-    
+    list_filter = ('created_at',)
     #튜플형태 () 와 리스트형태 [] 는 다르다. 튜플형태일 때 마지막에 콤마를 넣어야한다.
+    search_fields = ('id', )
+    #작성자를 넣게 된다면... lookup
+    #search_fields = ('id', 'writer__username') 언더바_ *2
+    search_help_text = '게시판 번호, 작성자 검색이 가능합니다.'
 
 admin.site.register(Comment)
